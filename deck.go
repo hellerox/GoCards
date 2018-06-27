a0300af5e673868c9ab886011a1ddc5331c08cb0
Carlos F <hellerox@gmail.com>
2018-06-27 18:02:39 -0500

    update
    creacion de nuevas funciones de ejemplo

--- deck.go
+++ deck.go
@@ -2,14 +2,55 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 //Create a new type of 'deck'
 //which is a slice of strings
 type deck []string
 
-func (cards deck) print() {
-	for i, card := range cards {
+func (d deck) print() {
+	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func deal(d deck, handsize int) (deck, deck) {
+	return d[:handsize], d[handsize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	//err sólo obtiene valor si un error ocurre, en caso contrario se quedaría con valor 'nil'
+
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	ss := strings.Split(string(bs), ",")
+	return deck(ss)
 }
